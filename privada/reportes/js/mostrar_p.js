48c19743d63f884f0a431ee80585343b9b9d43b7
samuel1205vc <samuelvalecalle@gmail.com>
2023-10-26 19:42:34 -0400

    cambio de color del titulo

--- privada/reportes/js/mostrar_p.js
+++
@@ -1,6 +0,0 @@
-"use strict"
-function mostrar(id_producto){
-    var d1, ventanaCalendario;
-    d1 = id_producto;
-    ventanaCalendario = window.open("ficha_tecnica_productos1.php?id_producto=" + d1 , "calendario", "width=600, height=550, left=100,top=100,scrollbars=yes,menubars=no,statusbar=NO,status=NO,resizable=YES,location=NO")
-}
